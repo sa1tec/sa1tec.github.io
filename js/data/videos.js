2441c210dfa1430b09cb5df72a830da7b1a6c844
Saidabrorkhon <abrorkhon1040@gmail.com>
2026-07-12 01:48:55 +0500

    Create videos.js

---
+++ js/data/videos.js
@@ -0,0 +1,21 @@
+// js/data/videos.js
+export const videos = [
+  {
+    title: 'Как рождается идея',
+    duration: '2:14',
+    poster: 'https://images.unsplash.com/photo-1492619375914-88005aa9e8fb?w=1000&q=70',
+    src: 'https://cdn.coverr.co/videos/coverr-working-on-a-laptop-9994/1080p.mp4'
+  },
+  {
+    title: 'Ночь перед релизом',
+    duration: '1:47',
+    poster: 'https://images.unsplash.com/photo-1517430816045-df4b7de11d1d?w=1000&q=70',
+    src: 'https://cdn.coverr.co/videos/coverr-typing-on-a-laptop-in-the-dark-9993/1080p.mp4'
+  },
+  {
+    title: 'Путешествие как перезагрузка',
+    duration: '3:02',
+    poster: 'https://images.unsplash.com/photo-1476514525535-07fb3b4ae5f1?w=1000&q=70',
+    src: 'https://cdn.coverr.co/videos/coverr-a-scenic-road-trip-2652/1080p.mp4'
+  }
+];
