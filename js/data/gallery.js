4f9a72ad846d5528c6cd657148bbad43c72780f3
Saidabrorkhon <abrorkhon1040@gmail.com>
2026-07-12 01:46:48 +0500

    Create gallery.js

---
+++ js/data/gallery.js
@@ -0,0 +1,11 @@
+// js/data/gallery.js
+export const gallery = [
+  { src: 'https://images.unsplash.com/photo-1500530855697-b586d89ba3ee?w=1000&q=70', alt: 'Горы на рассвете', size: 'big' },
+  { src: 'https://images.unsplash.com/photo-1493246507139-91e8fad9978e?w=1000&q=70', alt: 'Городской вечер', size: 'normal' },
+  { src: 'https://images.unsplash.com/photo-1517841905240-472988babdf9?w=1000&q=70', alt: 'Портрет', size: 'tall' },
+  { src: 'https://images.unsplash.com/photo-1476514525535-07fb3b4ae5f1?w=1000&q=70', alt: 'Дорога', size: 'normal' },
+  { src: 'https://images.unsplash.com/photo-1519681393784-d120267933ba?w=1000&q=70', alt: 'Архитектура', size: 'normal' },
+  { src: 'https://images.unsplash.com/photo-1523289333742-be1143f6b766?w=1000&q=70', alt: 'Побережье', size: 'normal' },
+  { src: 'https://images.unsplash.com/photo-1465101162946-4377e57745c3?w=1000&q=70', alt: 'Пустыня', size: 'normal' },
+  { src: 'https://images.unsplash.com/photo-1483728642387-6c3bdd6c93e5?w=1000&q=70', alt: 'Ночной город', size: 'tall' }
+];
