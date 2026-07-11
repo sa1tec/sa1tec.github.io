bdba7f7725872f3c9cb5ba15439b9f7ec33c45c7
Saidabrorkhon <abrorkhon1040@gmail.com>
2026-07-12 01:46:28 +0500

    Create friends.js

---
+++ js/data/friends.js
@@ -0,0 +1,9 @@
+// js/data/friends.js
+export const friends = [
+  { name: 'Илья', role: 'Партнёр по безумным идеям', img: 'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?w=500&q=70' },
+  { name: 'Алина', role: 'Всегда говорит правду', img: 'https://images.unsplash.com/photo-1544005313-94ddf0286df2?w=500&q=70' },
+  { name: 'Тимур', role: 'Друг с первого класса', img: 'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?w=500&q=70' },
+  { name: 'Мадина', role: 'Лучший критик моих проектов', img: 'https://images.unsplash.com/photo-1531123897727-8f129e1688ce?w=500&q=70' },
+  { name: 'Артур', role: 'С ним любое путешествие — приключение', img: 'https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?w=500&q=70' },
+  { name: 'Диана', role: 'Голос разума в 3 часа ночи', img: 'https://images.unsplash.com/photo-1520785643438-5bf77931f493?w=500&q=70' }
+];
